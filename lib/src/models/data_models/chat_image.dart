3e84d719408ce89e5028ef2b33b4c6a58130ffa9
muezz <muezz7055@gmail.com>
2025-01-01 21:13:27 +0500

    Image metadata

--- lib/src/models/data_models/chat_image.dart
+++ lib/src/models/data_models/chat_image.dart
@@ -10,6 +10,8 @@ class ChatImage {
   /// the file name as well so it can be used in the display or when someone
   /// tries to download the file
   final String name;
+  final String? ext;
+  final String? mimeType;
 
   /// File when it was selected by the user
   /// This will be null if this object comes from the backend
@@ -21,12 +23,16 @@ class ChatImage {
     required this.id,
     required this.name,
     required this.file,
+    this.ext,
+    this.mimeType,
   });
 
   ChatImage.fromPicker({
     required XFile this.file,
   })  : id = const UuidV4().generate(),
-        name = file.name;
+        name = file.name,
+        ext = file.path.split('.').lastOrNull,
+        mimeType = file.mimeType;
 
   // Image copyWith({
   //   String? url,
@@ -44,6 +50,8 @@ class ChatImage {
     return <String, dynamic>{
       'id': id,
       'name': name,
+      'ext': ext,
+      'mime_type': mimeType,
       // 'file': file?.toMap(),
     };
   }
@@ -52,20 +60,22 @@ class ChatImage {
     return ChatImage._(
       id: map['id'] as String,
       name: map['name'] as String,
+      ext: map['ext'] as String?,
+      mimeType: map['mime_type'] as String?,
       file: null,
     );
   }
 
   @override
-  String toString() => 'ChatImage(id: $id, name: $name, file: $file)';
+  String toString() => 'ChatImage(id: $id, name: $name, file: $file, ext: $ext, mimeType: $mimeType)';
 
   @override
   bool operator ==(covariant ChatImage other) {
     if (identical(this, other)) return true;
 
-    return other.id == id && other.name == name && other.file == file;
+    return other.id == id && other.name == name && other.file == file && other.ext == ext && other.mimeType == mimeType;
   }
 
   @override
-  int get hashCode => id.hashCode ^ name.hashCode ^ file.hashCode;
+  int get hashCode => id.hashCode ^ name.hashCode ^ file.hashCode ^ ext.hashCode ^ mimeType.hashCode;
 }
