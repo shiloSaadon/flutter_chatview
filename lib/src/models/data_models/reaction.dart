9b53dbfb43e1d2ce28bc63d6b10e2dd754e67721
muezz <muezz7055@gmail.com>
2025-01-13 21:36:50 +0500

    Add reaction copyWith

--- lib/src/models/data_models/reaction.dart
+++ lib/src/models/data_models/reaction.dart
@@ -1,60 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-
-// class Reaction {
-//   Reaction({
-//     required this.reactions,
-//     required this.reactedUserIds,
-//   });
-
-//   factory Reaction.fromJson(Map<String, dynamic> json) {
-//     final reactionsList = json['reactions'] is List<dynamic>
-//         ? json['reactions'] as List<dynamic>
-//         : <dynamic>[];
-
-//     final reactions = <String>[
-//       for (var i = 0; i < reactionsList.length; i++)
-//         if (reactionsList[i]?.toString().isNotEmpty ?? false)
-//           reactionsList[i]!.toString()
-//     ];
-
-//     final reactedUserIdList = json['reactedUserIds'] is List<dynamic>
-//         ? json['reactedUserIds'] as List<dynamic>
-//         : <dynamic>[];
-
-//     final reactedUserIds = <String>[
-//       for (var i = 0; i < reactedUserIdList.length; i++)
-//         if (reactedUserIdList[i]?.toString().isNotEmpty ?? false)
-//           reactedUserIdList[i]!.toString()
-//     ];
-
-//     return Reaction(
-//       reactions: reactions,
-//       reactedUserIds: reactedUserIds,
-//     );
-//   }
-
-//   /// Provides list of reaction in single message.
-//   final List<String> reactions;
-
-//   /// Provides list of user who reacted on message.
-//   final List<String> reactedUserIds;
-
-//   Map<String, dynamic> toJson() => {
-//         'reactions': reactions,
-//         'reactedUserIds': reactedUserIds,
-//       };
-
-//   Reaction copyWith({
-//     List<String>? reactions,
-//     List<String>? reactedUserIds,
-//   }) {
-//     return Reaction(
-//       reactions: reactions ?? this.reactions,
-//       reactedUserIds: reactedUserIds ?? this.reactedUserIds,
-//     );
-//   }
-// }
-
 class Reaction {
   final String idMessage;
   final String idUser;
@@ -93,4 +36,14 @@ class Reaction {
 
   @override
   int get hashCode => idMessage.hashCode ^ idUser.hashCode;
+
+  Reaction replaceReaction({
+    required String newReaction,
+  }) {
+    return Reaction(
+      idMessage: idMessage,
+      idUser: idUser,
+      reaction: newReaction,
+    );
+  }
 }
