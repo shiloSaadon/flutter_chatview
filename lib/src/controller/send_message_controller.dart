16d92e42a0a38e5dada971355f17aabd237b5ffa
shiloSaadon <shilosadon@gmail.com>
2025-04-16 20:15:02 +0300

    Add logic to send images with text

--- lib/src/controller/send_message_controller.dart
+++ lib/src/controller/send_message_controller.dart
@@ -53,6 +53,13 @@ class SendMessageController extends ChangeNotifier {
     resetReply();
   }
 
+  void removeImagesFromAttachment(List<ChatImage> images) {
+    Set<String> pates = images.map((e) => e.file?.path ?? '').toSet();
+    waitingForSendAttachmentsListener.value.removeWhere(
+        (element) => pates.contains((element as ChatImage).file?.path));
+    waitingForSendAttachmentsListener.notifyListeners();
+  }
+
   void onImageSelected(List<ChatImage> images) {
     debugPrint('Call in add image to attachments');
     for (var img in images) {
@@ -63,6 +70,21 @@ class SendMessageController extends ChangeNotifier {
     // resetReply();
   }
 
+  void sendImages(List<ChatImage> images, {String? caption}) {
+    debugPrint('Call in send images');
+    for (int i = 0; i < images.length; i++) {
+      final image = images[i];
+      onSendTap.call(
+          ImageMessage(
+              caption: i == images.length - 1 ? caption : null, image: image),
+          replyMessage);
+    }
+    waitingForSendAttachmentsListener
+      ..value = []
+      ..notifyListeners();
+    resetReply();
+  }
+
   void resetReply() {
     replyMessageListener.value = null;
   }
@@ -70,10 +92,21 @@ class SendMessageController extends ChangeNotifier {
   void onPressed() {
     final messageText = textEditingController.text.trim();
     textEditingController.clear();
-    if (messageText.isEmpty) return;
+    final textToSend = messageText.trim();
+    // Regular flow just send the message
+    if (waitingForSendAttachmentsListener.value.isEmpty) {
+      if (textToSend.isEmpty) return;
 
-    onSendTap.call(TextMessage(text: messageText.trim()), replyMessage);
-    resetReply();
+      onSendTap.call(TextMessage(text: textToSend), replyMessage);
+      resetReply();
+    }
+    // Attach the message to the last image
+    else {
+      sendImages(
+        waitingForSendAttachmentsListener.value as List<ChatImage>,
+        caption: textToSend,
+      );
+    }
   }
 
   void assignReplyMessage(UserMessage message, BuildContext context) {
